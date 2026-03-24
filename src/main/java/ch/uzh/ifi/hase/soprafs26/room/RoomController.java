a38dd96e0f78bf4250f4b9588d44e4168ad2a112
Raffael Bischoff <raffaelstefan.bischoff@uzh.ch>
2026-03-24 16:06:33 +0100

    add token auth logic

--- src/main/java/ch/uzh/ifi/hase/soprafs26/room/RoomController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/room/RoomController.java
@@ -1,6 +1,8 @@
 package ch.uzh.ifi.hase.soprafs26.room;
 
+import ch.uzh.ifi.hase.soprafs26.entity.User;
 
+import ch.uzh.ifi.hase.soprafs26.repository.UserRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
@@ -10,32 +12,38 @@ import java.util.List;
 @RestController
 public class RoomController {
 
-    private final RoomService RoomService;
+    private final RoomService roomService;
+    private final UserRepository UserRepository;
 
-    RoomController(RoomService RoomService) {
-        this.RoomService = RoomService;
+    RoomController(RoomService RoomService, UserRepository UserRepository) {
+        this.roomService = RoomService;
+        this.UserRepository = UserRepository;
+
     }
 
     @GetMapping("/rooms")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public List<Room> getRooms(@RequestHeader("token") String token) {
-        if (token == null) {
+        User userToken = UserRepository.findByToken(token);
+        if (userToken == null) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Token not found");
         }
-        if(RoomService.getAllRooms() == null){
+        if(roomService.getAllRooms() == null){
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "No Rooms present in database");
         }
-        return RoomService.getAllRooms();
+        return roomService.getAllRooms();
     }
 
     @GetMapping("/rooms/{id}")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
     public Room getRoom(@PathVariable Long id, @RequestHeader("token") String token) {
-        if (token == null) {
+        User userToken = UserRepository.findByToken(token);
+        if (userToken == null) {
             throw new ResponseStatusException(HttpStatus.UNAUTHORIZED, "Token not found");
         }
-        return RoomService.getRoomById(Long.toString(id));
+
+        return roomService.getRoomById(Long.toString(id));
     }
 }
