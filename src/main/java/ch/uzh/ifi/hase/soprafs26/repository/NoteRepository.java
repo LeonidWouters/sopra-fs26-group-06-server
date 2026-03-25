a2a407b414ee281c0a2b5f5fd5f184d8dc74810d
leonidwouters <leonid.wouters@uzh.ch>
2026-03-25 09:13:28 +0100

    created the NoteRepository

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/repository/NoteRepository.java
@@ -0,0 +1,15 @@
+package ch.uzh.ifi.hase.soprafs26.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import ch.uzh.ifi.hase.soprafs26.entity.Note;
+import java.util.List;
+import java.util.UUID;
+
+@Repository("noteRepository")
+public interface NoteRepository extends JpaRepository<Note, Long> {
+	List<Note> findBySessionId(UUID sessionId);
+
+	Note findById(Long id);
+}
