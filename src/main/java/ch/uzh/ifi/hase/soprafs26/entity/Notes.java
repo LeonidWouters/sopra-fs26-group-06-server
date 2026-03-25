091b619077dad9725111816200c83beea33029c3
leonidwouters <leonid.wouters@uzh.ch>
2026-03-25 08:58:33 +0100

    created the Notes entity

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/entity/Notes.java
@@ -0,0 +1,86 @@
+package ch.uzh.ifi.hase.soprafs26.entity;
+
+import jakarta.persistence.*;
+
+import org.springframework.cglib.core.Local;
+
+import java.io.Serializable;
+
+import java.time.LocalDateTime;
+
+/**
+ * Internal Note Representation
+ * This class composes the internal representation of the note and defines how
+ * the note is stored in the database.
+ * Every variable will be mapped into a database field with the @Column
+ * annotation
+ * - nullable = false -> this cannot be left empty
+ * - unique = true -> this value must be unqiue across the database -> composes
+ * the primary key
+ */
+@Entity
+@Table(name = "notes")
+public class Note implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@Column(nullable = false)
+	private String content;
+
+	@Column(nullable = false)
+	private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private UUID sessionId;
+
+	@PrePersist
+	protected void onCreate() {setCreationDate(LocalDateTime.now());}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreationDate(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public LocalDateTime getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(LocalDateTime updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public UUID getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(UUID sessionId) {
+        this.sessionId = sessionId;
+    }
+
+}
