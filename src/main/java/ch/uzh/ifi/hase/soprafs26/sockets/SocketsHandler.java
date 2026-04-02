3a7181dd000d831a4cb56b3a84f46693254d881f
Raffael Bischoff <raffaelstefan.bischoff@uzh.ch>
2026-04-02 07:52:18 +0200

    add verification logic to ws Handler

--- src/main/java/ch/uzh/ifi/hase/soprafs26/sockets/SocketsHandler.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/sockets/SocketsHandler.java
@@ -1,21 +1,36 @@
 package ch.uzh.ifi.hase.soprafs26.sockets;
 
 
+import ch.uzh.ifi.hase.soprafs26.entity.User;
+import ch.uzh.ifi.hase.soprafs26.repository.UserRepository;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-import tools.jackson.databind.ObjectMapper;
+
 
 
 public class SocketsHandler extends TextWebSocketHandler {
+    private final UserRepository userRepository;
+
+    public SocketsHandler(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     public void afterConnectionEstablished(WebSocketSession session) throws Exception {
-        System.out.println(session.getUri());
+       String query = session.getUri().getQuery();//get query param, separate check to avoid null pointer
 
-        TextMessage greeting = new TextMessage("Connected to " + session.getRemoteAddress());
+       String token = null;
+       if(query != null && query.contains("token=")) {
+           token = query.split("token=")[1];
+       }
 
-        session.sendMessage(greeting);
+       if(token == null | userRepository.findByToken(token) == null) {
+           session.close(CloseStatus.POLICY_VIOLATION);//close session before first message is sent if user is not authenticated
+           return;
+       }
+       session.sendMessage(new TextMessage("connected to" + session.getLocalAddress()));
 
     }
     @Override
