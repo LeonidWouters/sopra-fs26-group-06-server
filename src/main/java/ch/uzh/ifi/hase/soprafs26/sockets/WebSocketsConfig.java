bdb4383f440a50c804b3832ca312dadc3a6a2cfb
leonidwouters <leonid.wouters@uzh.ch>
2026-04-02 15:17:55 +0200

    added wiring session manager into websocket handler. added join/leave lifecycle messages.

--- src/main/java/ch/uzh/ifi/hase/soprafs26/sockets/WebSocketsConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/sockets/WebSocketsConfig.java
@@ -1,27 +1,33 @@
 package ch.uzh.ifi.hase.soprafs26.sockets;
 
+import ch.uzh.ifi.hase.soprafs26.repository.UserRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 
 @Configuration
 @EnableWebSocket
 class WebSocketsConfig implements WebSocketConfigurer {
 
+	private final WebSocketHandler socketsHandler;
+
+	WebSocketsConfig(WebSocketHandler socketsHandler) {
+		this.socketsHandler = socketsHandler;
+	}
+
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(SocketsHandler(), "/ws/SocketsHandler")
+        registry.addHandler(socketsHandler, "/ws/SocketsHandler")
                 .setAllowedOrigins("*");
     }
 
     @Bean
-    public WebSocketHandler SocketsHandler() {
-        return new SocketsHandler();
+    public WebSocketHandler socketsHandler(UserRepository userRepository, SessionManager sessionManager) {
+        return new SocketsHandler(userRepository, sessionManager);
     }
 
 }
