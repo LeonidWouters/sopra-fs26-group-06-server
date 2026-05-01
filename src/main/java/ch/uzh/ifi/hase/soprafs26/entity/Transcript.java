e1640e27c8c0309ea3afa6dc5a0f6a3b51b38d9d
leonidwouters <leonid.wouters@uzh.ch>
2026-05-01 11:07:46 +0200

    changed user entities to enable longer texts for transcripts, notes and user bio.

--- src/main/java/ch/uzh/ifi/hase/soprafs26/entity/Transcript.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/entity/Transcript.java
@@ -21,7 +21,7 @@ public class Transcript implements Serializable {
     @GeneratedValue
     private Long id;
 
-    @Column(nullable = false)
+    @Column(nullable = false, columnDefinition = "TEXT")
     private String content;
 
     @Column(nullable = false)
